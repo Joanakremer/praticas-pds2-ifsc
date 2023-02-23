b1e28266f5b08887533214bb7cff9a2e6b47c582
joana.kt31@aluno.ifsc.edu.br <joana.kt31@aluno.ifsc.edu.br>
2023-02-23 10:24:02 -0300

    correção aula 03

--- pratica-01/src/controle/PessoaDAO.java
+++ pratica-01/src/controle/PessoaDAO.java
@@ -1,4 +1,6 @@
-package controle; 
+
+package controle;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -9,58 +11,79 @@ import java.util.ArrayList;
 import modelo.Pessoa;
 
 public class PessoaDAO {
+
 	private Conexao con;
-	
-	public boolean inserir(Pessoa p) {
-		
-		//Instacia Conexao//
+
+	public boolean Inserir(Pessoa pessoa) {
 		con = Conexao.getInstancia();
-		
-		//Abrir Conexao//
 		Connection c = con.conectar();
-		try { 
-			String 	query = "UPDATE pessoa SET nome = ? WHERE cpf = ?";
+		try {
+			String query = "INSERT into pessoa(cpf, nome) VALUES(?,?);";
 			PreparedStatement stm = c.prepareStatement(query);
-			stm.setString(1,  p.getNome());
-			stm.setString(2,  p.getNome());
-			stm.executeUpdate();
+			stm.setLong(1, pessoa.getCpf());
+			stm.setString(2, pessoa.getNome());
+			int valida = stm.executeUpdate();
 			return true;
-		
-	}catch (SQLException e) {
-		e.printStackTrace();
-	}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		con.fecharConexao();
 		return false;
-	}
-		
-	
-	public boolean atualizar(Pessoa p) {
-		return false;
-	}
-	
-	public boolean deletar(Pessoa p) {
-		return false;
-		
-	}
-	public ArrayList<Pessoa> listarPessoa(){
-		//Instacia Conexao//
-		con = Conexao.getInstancia();
-		
-		//Abrir Conexao//
-		Connection c = con.conectar();
-		try { 
-			Statement stm = c.createStatement();
-			String query = "DELETE FROM PESSOA where cpf = ?";
-			ResultSet rs = stm.executeQuery(query);
-			while (rs.next()) {
-				int id = rs.getInt("id");
-				
-			}
-		
-	}catch (SQLException e) {
-		e.printStackTrace();
-	}
-		return null;
 	}
 
+	public boolean alterar(Pessoa pessoa) {
+		Connection conn = Conexao.getInstancia().conectar();
+		try {
+			String query = "UPDATE pessoa SET nome = ? WHERE cpf = ?";
+			PreparedStatement stm = conn.prepareStatement(query);
+			stm.setString(1, "Leonardo Ribeiro");
+			stm.setLong(2, pessoa.getCpf());
+			int valida = stm.executeUpdate();
+			return true;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		con.fecharConexao();
+		return false;
+	}
+
+	public boolean deletar(Pessoa pessoa) {
+		Connection conn = Conexao.getInstancia().conectar();
+		try {
+			String query = "DELETE FROM pessoa WHERE cpf = ?";
+			PreparedStatement stm = conn.prepareStatement(query);
+			stm.setLong(1, pessoa.getCpf());
+			int valida = stm.executeUpdate();
+			return true;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		con.fecharConexao();
+		return false;
+	}
+
+	public ArrayList<Pessoa> listarPessoas() {
+		ArrayList<Pessoa> pessoas = new ArrayList<>();
+		con = Conexao.getInstancia();
+		Connection c = con.conectar();
+		try {
+			Statement stm = c.createStatement();
+			String query = "Select * from pessoa";
+			ResultSet rs = stm.executeQuery(query);
+			while (rs.next()) {
+				int cpf = rs.getInt("cpf");
+				String nome = rs.getString("nome");
+				Pessoa p = new Pessoa();
+				p.setCpf(cpf);
+				p.setNome(nome);
+				pessoas.add(p);
+
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		con.fecharConexao();
+		return pessoas;
+	}
 
 }
--- pratica-01/src/visão/tela.java
+++ pratica-01/src/visão/tela.java
@@ -11,6 +11,7 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Color;
 import java.awt.Font;
+import javax.swing.JLabel;
 
 public class tela extends JFrame {
 
@@ -39,8 +40,9 @@ public class tela extends JFrame {
 	 */
 	public tela() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 556, 333);
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(0, 128, 128));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
@@ -48,24 +50,41 @@ public class tela extends JFrame {
 		
 		txtNome = new JTextField();
 		txtNome.setFont(new Font("Source Serif Pro", Font.PLAIN, 14));
-		txtNome.setBounds(89, 89, 237, 33);
+		txtNome.setBounds(160, 86, 237, 33);
 		contentPane.add(txtNome);
 		txtNome.setColumns(10);
 		
 		textField_1 = new JTextField();
 		textField_1.setFont(new Font("Source Serif Pro", Font.PLAIN, 14));
-		textField_1.setBounds(89, 146, 237, 33);
+		textField_1.setBounds(160, 152, 237, 33);
 		contentPane.add(textField_1);
 		textField_1.setColumns(10);
 		
 		JButton btnNewButton = new JButton("Cadastrar");
+		btnNewButton.setBackground(new Color(192, 192, 192));
 		btnNewButton.setFont(new Font("Source Serif Pro", Font.PLAIN, 14));
-		btnNewButton.setForeground(new Color(51, 102, 255));
+		btnNewButton.setForeground(new Color(0, 0, 0));
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		btnNewButton.setBounds(169, 207, 112, 23);
+		btnNewButton.setBounds(218, 208, 112, 23);
 		contentPane.add(btnNewButton);
+		
+		JLabel lblNewLabel = new JLabel("Nome:");
+		lblNewLabel.setFont(new Font("Source Serif Pro", Font.PLAIN, 14));
+		lblNewLabel.setBounds(104, 95, 46, 14);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("CPF:");
+		lblNewLabel_1.setFont(new Font("Source Serif Pro", Font.PLAIN, 14));
+		lblNewLabel_1.setBounds(104, 162, 46, 14);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("Cadastro ");
+		lblNewLabel_2.setBackground(new Color(0, 0, 0));
+		lblNewLabel_2.setFont(new Font("Source Serif Pro", Font.PLAIN, 30));
+		lblNewLabel_2.setBounds(195, 22, 147, 53);
+		contentPane.add(lblNewLabel_2);
 	}
 }
